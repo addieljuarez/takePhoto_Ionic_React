566a276f058ec57445c0f67117f5ce981ca8e4d8
Addiel Juarez <addieljuarez@gmail.com>
2021-07-23 23:28:02 -0500

    se agrega platforms

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { isPlatform } from "@ionic/react";
 import {
     Camera,
+    CameraPhoto,
     CameraResultType,
     CameraSource,
     Photo,
@@ -15,11 +16,50 @@ import { Capacitor } from '@capacitor/core';
 
 
 
-
+const PHOTO_STORAGE = 'photos';
 
 export function usePhotoGallery(){
-    
+
     const [photos, setPhotos] = useState <UserPhoto[]>([]);
+    
+
+    const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+        const base64Data = await base64FromPath(photo.webPath!);
+        const savedFile = await Filesystem.writeFile({
+            path: fileName,
+            data: base64Data,
+            directory: Directory.Data,
+        });
+
+        return {
+            filepath: fileName,
+            webviewPath: photo.webPath
+        };
+    }
+
+
+    useEffect(()=>{
+        const loadSave = async () => {
+            const { value } = await Storage.get({key: PHOTO_STORAGE});
+            const photosInStorage = (value ? JSON.parse(value) : []) as UserPhoto[];
+
+            if (!isPlatform('hybrid')) {
+                for (let photo of photosInStorage) {
+                    const file = await Filesystem.readFile({
+                        path: photo.filepath,
+                        directory: Directory.Data
+                    });
+                    // Web platform only: Load the photo as base64 data
+                    photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+                }
+            }
+
+            setPhotos(photosInStorage);
+        }
+        loadSave();
+    }, []);
+  
+
     const takePhoto = async () => {
         const cameraPhoto = await Camera.getPhoto({
             resultType: CameraResultType.Uri,
@@ -28,14 +68,20 @@ export function usePhotoGallery(){
         });
 
         const fileName = new Date().getTime() + '.jpeg';
-        const newPhotos = [
-            {
-                filepath: fileName,
-                webviewPath: cameraPhoto.webPath,
-            },
-            ...photos
-        ]
+        const savedFileImage = await savePicture(cameraPhoto, fileName)
+        // const newPhotos = [
+        //     {
+        //         filepath: fileName,
+        //         webviewPath: cameraPhoto.webPath,
+        //     },
+        //     ...photos
+        // ]
+        const newPhotos = [savedFileImage, ...photos];
         setPhotos(newPhotos);
+        Storage.set({
+            key: PHOTO_STORAGE,
+            value: JSON.stringify(newPhotos)
+        });
 
         
     }
@@ -47,7 +93,60 @@ export function usePhotoGallery(){
 }
 
 
+const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+    let base64Data: string;
+    // "hybrid" will detect Cordova or Capacitor;
+    if (isPlatform('hybrid')) {
+        const file = await Filesystem.readFile({
+            path: photo.path!
+        });
+        base64Data = file.data;
+    } else {
+        base64Data = await base64FromPath(photo.webPath!);
+    }
+    const savedFile = await Filesystem.writeFile({
+        path: fileName,
+        data: base64Data,
+        directory: Directory.Data
+    });
+  
+    if (isPlatform('hybrid')) {
+        // Display the new image by rewriting the 'file://' path to HTTP
+        // Details: https://ionicframework.com/docs/building/webview#file-protocol
+        return {
+            filepath: savedFile.uri,
+            webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+        };
+    }
+    else {
+        // Use webPath to display the new image instead of base64 since it's
+        // already loaded into memory
+        return {
+            filepath: fileName,
+            webviewPath: photo.webPath
+        };
+    }
+};
+
+
 export interface UserPhoto{
     filepath: string;
     webviewPath?: string;
+}
+
+export async function base64FromPath(path: string): Promise<string> {
+    const response = await fetch(path);
+    const blob = await response.blob();
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = () => {
+            if (typeof reader.result === 'string') {
+            resolve(reader.result);
+            } else {
+            reject('method did not return a string')
+            }
+        };
+        reader.readAsDataURL(blob);
+    });
 }
